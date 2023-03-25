8c7766a7c851aec8df874ea835d406694ac1081b
s403o <eslam.adel.me@gmail.com>
2023-03-26 01:39:40 +0200

    add author model & one to many relation

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.core.validators import MinValueValidator
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
 class Post(models.Model):
     title = models.CharField(max_length=100)
     image_name = models.CharField(max_length=100)
@@ -9,3 +13,5 @@ class Post(models.Model):
     content = models.TextField(validators=[MinValueValidator(10)])
     slug = models.SlugField(unique=True) # db_index=True auto set
     excerpt = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    
