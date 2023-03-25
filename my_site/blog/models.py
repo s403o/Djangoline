6cde65f553bd5b89fdadac6e43febeccdd0f82c1
s403o <eslam.adel.me@gmail.com>
2023-03-26 01:34:33 +0200

    add post model

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    content = models.TextField(validators=[MinValueValidator(10)])
+    slug = models.SlugField(unique=True) # db_index=True auto set
+    excerpt = models.CharField(max_length=200)
