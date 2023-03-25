fb74de7eee3341c17a51573607ea2a5f10a1a6f6
s403o <eslam.adel.me@gmail.com>
2023-03-26 01:47:47 +0200

    fix issue

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -15,6 +15,6 @@ class Post(models.Model):
     content = models.TextField(validators=[MinValueValidator(10)])
     slug = models.SlugField(unique=True) # db_index=True auto set
     excerpt = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
     
