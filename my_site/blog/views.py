962538c62aa0066e0d8d1766fb1313cda1cf6d63
s403o <eslam.adel.me@gmail.com>
2023-03-26 17:03:26 +0200

    fetching data

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,68 +1,24 @@
-from django.shortcuts import render
-from datetime import date
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
 
-all_posts = [
-  {
-      "slug": "devops-culture",
-      "image": "DevOps-lifecycle.png",
-      "author": "s403o",
-      "date": date(2023, 2, 23),
-      "title": "DevOps Culture",
-      "excerpt": "DevOps is the combination of cultural philosophies, practices, and tools that increases an organization's ability to deliver applications and services at high velocity.",
-      "content": """
-  DevOps is the combination of cultural philosophies, practices, and tools that increases an organizations ability to deliver applications and services at high velocity: evolving and improving products at a faster pace than organizations using traditional software development and infrastructure management processes. This speed enables organizations to better serve their customers and compete more effectively in the market.
-"""
-  },
-
-  {
-      "slug": "devops-roadmap",
-      "image": "devops-roadmap.png",
-      "author": "s403o",
-      "date": date(2023, 2, 22),
-      "title": "DevOps Roadmap",
-      "excerpt": "DevOps is the combination of cultural philosophies, practices, and tools that increases an organization's ability to deliver applications and services at high velocity.",
-      "content": """
-  DevOps is the combination of cultural philosophies, practices, and tools that increases an organizations ability to deliver applications and services at high velocity: evolving and improving products at a faster pace than organizations using traditional software development and infrastructure management processes. This speed enables organizations to better serve their customers and compete more effectively in the market.
-"""
-  },
-
-  {
-      "slug": "Site-Reliability-Engineering",
-      "image": "sre.png",
-      "author": "s403o",
-      "date": date(2023, 2, 24),
-      "title": "SRE",
-      "excerpt": "SRE is what you get when you treat operations as if it’s a software problem.",
-      "content": """
-  SRE is what you get when you treat operations as if it’s a software problem. Our mission is to protect, provide for, and progress the software and systems behind all of Google’s public services — Google Search, Ads, Gmail, Android, YouTube, and App Engine, to name just a few — with an ever-watchful eye on their availability, latency, performance, and capacity.
-"""
-  },
-]
-
-
-def get_date(post):
-  return post['date']
-
-
 def starting_page(request):
-  # sorted_posts = all_posts.sort(key=get_date) # sort existing list
-  sorted_posts = sorted(all_posts, key=get_date) # sort to new list
-  latest_posts = sorted_posts[-3:]
+  latest_posts = Post.objects.all().order_by('-date')[:3]
   return render(request, "blog/index.html", {
     "posts": latest_posts,
   })
 
 
 def posts(request):
+  all_posts = Post.objects.all().order_by('-date')
   return render(request, "blog/all-posts.html", {
     "all_posts": all_posts
   })
 
 
-def post_details(request, slug):  # slug (dynamic segment)
-  post_id = next(post for post in all_posts if post['slug'] == slug)
+def post_details(request, slug):
+  identified_post = get_object_or_404(Post, slug=slug) # Post.objects.get(slug=slug)
   return render(request, "blog/post-details.html", {
-    "post": post_id
+    "post": identified_post,
+    "post_tags": identified_post.tags.all()
   })
